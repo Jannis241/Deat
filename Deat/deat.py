8df994953fdfde1697591b17d01b37cc56850f05
Jannis241 <jannis2401p@gmail.com>
2024-06-24 22:13:42 +0200

    Add files via upload

---
+++ Deat/deat.py
@@ -0,0 +1,32 @@
+import math
+
+
+class Neuron():
+    def __init__(self) -> None:
+        pass
+
+
+class Layer():
+    def __init__(self) -> None:
+        pass
+
+
+class NeuralNetork():
+    def __init__(self, numberOfInputNodes, Hiddenlayers, numberOfOutputNodes):
+        self.inputs = numberOfInputNodes
+        self.hiddenLayer = Hiddenlayers
+        self.outputNodes = numberOfOutputNodes
+        self.score = 0
+
+    def mutate(self):
+        pass
+
+    def getBestNN(self):
+        pass
+
+    def calcOutput(self):
+        pass
+
+
+
+
---
+++ Deat/example.py
@@ -0,0 +1,5 @@
+import deat
+
+
+NN = deat.NeuralNetork(2, [3,3], 2)
+NN.start()
